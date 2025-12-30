090ea4b803d7cf69f4b3b6603aab2b9e683b036f
KALP-1609 <kalp.b0450@gmail.com>
2025-12-30 13:02:37 +0530

    Handle POST form submission with scaled inputs for model prediction

--- application.py
+++ application.py
@@ -20,7 +20,20 @@ def index():
 @app.route('/predict',methods=['GET','POST'])
 def predict_data():
     if request.method == 'POST':
-        pass
+        temperature = float(request.form.get('Temperature'))
+        rh = float(request.form.get('RH'))
+        ws = float(request.form.get('WS'))
+        rain = float(request.form.get('Rain'))
+        ffmc = float(request.form.get('FFMC'))
+        dmc = float(request.form.get('DMC'))
+        isi = float(request.form.get('ISI'))
+        classes = float(request.form.get('Classes'))
+        region = float(request.form.get('Region'))
+
+        scaled_data = standard_scaler.transform([[temperature, rh, ws, rain, ffmc, dmc, isi, classes, region]])
+        result = ridge_model.predict(scaled_data)
+
+        return render_template('home.html',results=result[0])
     else:
         return render_template('home.html')
 
